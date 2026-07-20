1a705fc161308d858506b1ab5af00e4f02b309cf
learner <learner@users.noreply.github.com>
2026-07-20 14:02:02 -0500

    nesLoops

---
+++ day1-python/nesLoops.py
@@ -0,0 +1,19 @@
+# grid = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# total = 0
+# items = 0
+# for i in range(len(grid)):
+#     for j in range(len(grid[i])):
+#         total += grid[i][j]
+#         items+=1
+
+# print(total)
+# print(items)
+
+for i in range(1,4):
+    for j in range(1,5):
+        print(i, j)
+
