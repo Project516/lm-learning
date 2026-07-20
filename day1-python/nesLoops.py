98d3c2a28b8416e2662328228e33983e118dc845
project516 <138796702+Project516@users.noreply.github.com>
2026-07-20 14:16:14 -0500

    finish 9.1

--- day1-python/nesLoops.py
+++ day1-python/nesLoops.py
@@ -13,7 +13,30 @@
 # print(total)
 # print(items)
 
-for i in range(1,4):
-    for j in range(1,5):
-        print(i, j)
+# for i in range(1,4):
+#     for j in range(1,5):
+#         print(i, j)
+
+# task 3
+# for i in range(2,5):
+#     for j in range(1,6):
+#         print(i, " * ", j, " = ", i * j)
+
+# task 4
+
+# def isEven(num):
+#     if (num % 2 == 0):
+#         return True
+#     else:
+#         return False
+
+string = ""
+for i in range(0,4):
+    for j in range(0,4):
+        if((i+j)%2 ==0):
+            string += "#"
+        else:
+            string += "."
+    string += "\n"
+print(string)
 
