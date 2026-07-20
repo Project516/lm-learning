072e6635987315f2859e25a8681b66af59773570
Academy of Game Dev Camper <horns602@cs.utexas.edu>
2026-07-20 10:59:20 -0500

    second cp

--- day1-python/hello.py
+++ day1-python/hello.py
@@ -2,4 +2,24 @@
 
 #print("Burnt orange and white,\nLonghorns stand with pride and might,\nHook em, Texas fight!")
 
-print("  @..@\n (----)\n(>____<)\n^^~~~~^^")
+#print("  @..@\n (----)\n(>____<)\n^^~~~~^^")
+
+# student_name = "Alex"
+
+# print(student_name + " was a dedicated computer science student at UT Austin.")
+# print("Each morning " + student_name + " biked through the vibrant UT campus.")
+# print(student_name + " enjoyed all of the interesting CS classes.")
+# print("During the week, "+ student_name + " would meet up with friends to collaborate on challenging programming projects.")
+# print("On weekends, " +student_name+ " participated in hackathons.")
+# print("Computer science allowed "+student_name+ " to think logically and creatively.")
+
+# name = "Sam"
+# age = 16
+# print(name)
+# print(age)
+
+# age = 17
+# favorite_color = "green"
+
+# print(age)
+# print(favorite_color + "\nMy favorites are ", age , " " +favorite_color)
