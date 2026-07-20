cd450462447fcca331e3b2dad682f0145727b3c0
learner <learner@users.noreply.github.com>
2026-07-20 13:39:38 -0500

    while loops

---
+++ day1-python/while.py
@@ -0,0 +1,12 @@
+# num = 10
+
+# while num>0:
+#     print(num)
+#     num-=1
+
+# print("Done!")
+names = ["Sam", "Alex", "Jo"]
+num = 0
+while (num<len(names)):
+    print(num, names[num])
+    num+=1
