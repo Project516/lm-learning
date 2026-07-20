c46f47f7f34796e278fb719076eb6d10513ca246
learner <learner@users.noreply.github.com>
2026-07-20 11:14:32 -0500

    functions

---
+++ day1-python/functions.py
@@ -0,0 +1,50 @@
+# def say_hello():
+
+#     print("Hello, Academy!")
+
+# say_hello()
+# say_hello()
+
+# def greet(person):
+#     print("Hello, " + person + "!")
+
+# greet("Sam")
+# greet("Alex")
+
+# def greet(person):
+#     """Prints a friendly hello to one person.
+
+#     Args:
+#         person: The name to greet, as a string.
+#     """
+#     print("Hello, " + person + "!")
+
+# greet("Sam")
+
+# def introduce(person):
+
+#     """prints the person's name
+
+#     Args:
+#         person: the name string of the person
+#     """
+#     print("My name is", person)
+
+# def cheer():
+#     """cheers on the team
+#     """
+#     print("Go team!")
+
+# introduce("Joe")
+# introduce("Bob")
+# cheer()
+
+def square(num):
+    """Returns the number squared
+    """
+    return num*num
+
+print(square(5))
+print(square(square(2)))
+
+
