2bcc1e136e58c967732fa415e8f58092e89dcd15
Academy of Game Dev Camper <horns602@cs.utexas.edu>
2026-07-20 15:42:10 -0500

    loops thing

---
+++ day1-python/cleanerLoops.py
@@ -0,0 +1,12 @@
+letters = ["a", "b", "c"]
+
+for index, letter in enumerate(letters):
+    print(index, letter)
+
+prices = [4, 9, 2]
+quantities = [2, 1, 5]
+
+for price, quantity in zip(prices, quantities):
+    print(price*quantity)
+
+
