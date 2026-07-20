d402b8470ba0f256c1ecc8dab79a01c4e409fd6f
project516 <138796702+Project516@users.noreply.github.com>
2026-07-20 15:07:47 -0500

    a.3

--- day1-python/dicts.py
+++ day1-python/dicts.py
@@ -1,5 +1,29 @@
-friends = {"Ben" : 16, "Joe" : 17, "Bob" : 18}
-friends["Kai"] = 19
+# friends = {"Ben" : 16, "Joe" : 17, "Bob" : 18}
+# friends["Kai"] = 19
 
-print(friends)
-print(friends.get("Jo", 0))
+# print(friends)
+# print(friends.get("Jo", 0))
+
+# string = "mississippi"
+
+# counts = {}
+# for char in string:
+#     counts[char] = counts.get((char), 0) + 1
+
+# for char, number in counts.items():
+#     if (number > 2):
+#         print(char)
+
+# print(counts)
+
+# words = ["hi", "hello", "hey", "howdy", "yo"]
+
+# big = [ words[n] for n in range(len(words)) if (len(words[n])>3)]
+
+# print(big)
+
+words = ["hi", "hello", "hey", "howdy", "yo"]
+
+big = [ len(word) for word in words]
+
+print(big)
