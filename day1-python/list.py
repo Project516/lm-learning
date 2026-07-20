a2be0744b07ae6e8f70843cd58596a78ea6733f7
project516 <138796702+Project516@users.noreply.github.com>
2026-07-20 11:24:09 -0500

    lists

---
+++ day1-python/list.py
@@ -0,0 +1,35 @@
+# animals = ["bear", "ant", "horse"]
+
+# print(animals[0])
+# print(animals[1])
+
+# print(animals[-1])
+
+# longhorn_points = [38, 45, 21, 31, 27]
+# opponent_points = [20, 14, 24, 10, 34]
+
+# nums = []
+
+# nums.append(1)
+# nums.append(2)
+# nums.append(3)
+
+# print(nums)
+# print(len(nums))
+# print(nums[len(nums) - 1])
+
+# longhorn_points.append(42)
+# opponent_points.append(7)
+
+# print(longhorn_points)
+# print(opponent_points)
+
+# print(len(longhorn_points))
+# print(len(opponent_points))
+
+grid = [
+    [1, 2],
+    [3, 4]
+]
+print(grid[0])
+print(grid[1][0])
