e5a9ee6c1e2fbf971e7ab4cc1280aea6d4e81c3a
project516 <138796702+Project516@users.noreply.github.com>
2026-07-20 11:57:32 -0500

    loops

---
+++ day1-python/longhorn.py
@@ -0,0 +1,45 @@
+longhorn_points = [38, 45, 21, 31, 27]
+opponent_points = [20, 14, 24, 10, 34]
+
+
+longhorn_points.append(42)
+opponent_points.append(7)
+
+
+if(longhorn_points[0]>opponent_points[0]):
+    print("win")
+else:
+    print("lose")
+
+total = 0
+for lp in longhorn_points:
+    total += lp
+
+print(total)
+
+optotal = 0
+for op in opponent_points:
+    optotal += op
+
+print(optotal)
+
+
+biggest = longhorn_points[0]
+
+for n in longhorn_points:
+    if n > biggest:
+        biggest = n
+
+print(biggest)
+
+total = 0
+
+
+for i in range(len(longhorn_points)):
+    if longhorn_points[i] > opponent_points[i]:
+        print(i, "win")
+        total += 1
+    else:
+        print(i, "loss")
+
+print(total)
