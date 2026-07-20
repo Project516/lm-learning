519296021bdb441b8e8a972fac4936443b75b8a8
learner <learner@users.noreply.github.com>
2026-07-20 16:00:33 -0500

    commented out

--- day1-python/classes.py
+++ day1-python/classes.py
@@ -11,30 +11,30 @@
 # myStudent1 = Student("Jimmy")
 # myStudent1.hello()
 
-class BankAccount:
-    def __init__(self, name, account_number):
-        self.name = name
-        self.account_number = account_number
-        self.balance = 0
+# class BankAccount:
+#     def __init__(self, name, account_number):
+#         self.name = name
+#         self.account_number = account_number
+#         self.balance = 0
 
-    def deposit(self, amount):
-        self.balance += amount
+#     def deposit(self, amount):
+#         self.balance += amount
 
-    def withdraw(self, amount):
-        if(self.balance >= amount):
-            self.balance -= amount
-        else:
-            print("insufficient funds")
+#     def withdraw(self, amount):
+#         if(self.balance >= amount):
+#             self.balance -= amount
+#         else:
+#             print("insufficient funds")
     
-    def transfer(self, other_acct, amount):
-        if(self.balance >= amount):
-            self.balance -= amount
-            other_acct.balance += amount
-        else:
-            print("insufficent funds")
-tom_acct = BankAccount("Tom", 1234)
-mary_acct = BankAccount("Mary", 5678)
-tom_acct.deposit(100)
-tom_acct.transfer(mary_acct, 40)
-print(tom_acct.balance)
-print(mary_acct.balance)
+#     def transfer(self, other_acct, amount):
+#         if(self.balance >= amount):
+#             self.balance -= amount
+#             other_acct.balance += amount
+#         else:
+#             print("insufficent funds")
+# tom_acct = BankAccount("Tom", 1234)
+# mary_acct = BankAccount("Mary", 5678)
+# tom_acct.deposit(100)
+# tom_acct.transfer(mary_acct, 40)
+# print(tom_acct.balance)
+# print(mary_acct.balance)
