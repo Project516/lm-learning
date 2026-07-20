b5c80cddb29620de58d645a79fe37e7d76e59d8d
Academy of Game Dev Camper <horns602@cs.utexas.edu>
2026-07-20 13:48:37 -0500

    while loops

--- day1-python/while.py
+++ day1-python/while.py
@@ -15,14 +15,24 @@
 #     num+=1
 
 # task 3
-list = [1,2,3,4]
-tempList = list
-#counter = 0
-total = 0
+# list = [1,2,3,4]
+# tempList = list
+# #counter = 0
+# total = 0
 
-while (len(tempList) != 0):
-    total += tempList[0]
-    tempList.pop(0)
+# while (len(tempList) != 0):
+#     total += tempList[0]
+#     tempList.pop(0)
+
+# print(total)
+
+#task 4
+list = [1, -2, 3, 4]
+counter = 0
+total = 0
+while (counter < len(list)):
+    if(list[counter]>0):
+        total += list[counter]
+    counter +=1
 
 print(total)
-
